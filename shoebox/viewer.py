a6c2f6baec822792bc2bd1d0b82740ba29ad5f29
Marty Sauser <msauser2@gmail.com>
2018-06-01 09:45:00 -0400

    Add split mode

--- shoebox/viewer.py
+++ shoebox/viewer.py
@@ -13,6 +13,153 @@ from tkit.widgethelper import WidgetHelper
 instances = []
 nextInstNum = 1
 
+class Pane:
+    def __init__(self, viewer, side):
+        self.viewer = viewer
+        self.px = viewer.px
+        self.side = side
+        self.selectColor = len(selectColors)
+        if side == RIGHT:
+            self.selectColor -= 1
+
+        self.prevButton = ttk.Button(self.viewer.topBar, text="<<", command=self.do_prev)
+        self.nextButton = ttk.Button(self.viewer.topBar, text=">>", command=self.do_next)
+        if side == LEFT:
+            self.prevButton.pack(side=side)
+            self.nextButton.pack(side=side)
+        else:
+            self.nextButton.pack(side=side)
+            self.prevButton.pack(side=side)
+
+        # create canvas
+        self.canvas = Canvas(self.viewer.canvasFrame, background="black",
+                             highlightthickness=1, highlightbackground="black",
+                             highlightcolor=selectColors[self.selectColor])
+        self.canvas.pack(side=side, fill=BOTH, expand=True)
+
+        # canvas stuff
+        self.canvas.bind('<Configure>', self.on_canvas_resize)
+        self.canvas.bind('<MouseWheel>', self.on_canvas_wheel)
+        self.canvas.bind('<Button-1>', self.on_canvas_click)
+        self.canvas.bind('<Double-Button-1>', self.on_canvas_doubleclick)
+        self.canvas.bind('<B1-Motion>', self.on_canvas_motion)
+        self.canvas.bind('<ButtonRelease>', self.on_canvas_release)
+
+        self.index = 0
+        self.fullImg = None
+        self.cropImg = None
+        self.tkPhoto = None
+        self.imgItem = None
+        self.zoom = 1.0
+        self.panx = 0
+        self.pany = 0
+        self.dragging = False
+        self.dragStart = None
+
+    def on_canvas_resize(self, event):
+        """when user resizes the window"""
+        self.draw_image()
+
+    def on_canvas_wheel(self, event):
+        zbefore = self.zoom
+        self.zoom += event.delta / 2000.0
+        if self.zoom <= 0:
+            self.zoom = zbefore
+        else:
+            self.panx *= self.zoom / zbefore
+            self.pany *= self.zoom / zbefore
+            self.draw_image()
+
+    def on_canvas_click(self, event):
+        self.canvas.focus_set()
+
+    def on_canvas_doubleclick(self, event):
+        self.zoom = 1.0
+        self.panx = 0
+        self.pany = 0
+        self.draw_image()
+
+    def on_canvas_motion(self, event):
+        """when mouse moved with button down"""
+        if not self.dragging:
+            self.dragging = True
+            self.dragStart = (self.panx + event.x, self.pany + event.y)
+            self.canvas.configure(cursor="fleur")
+        else:
+            self.panx = self.dragStart[0] - event.x
+            self.pany = self.dragStart[1] - event.y
+            self.draw_image()
+
+    def on_canvas_release(self, event):
+        """when mouse button released"""
+        self.dragging = False
+        self.canvas.configure(cursor="")
+
+    def set_picture(self, index):
+        self.index = index
+        tile = self.px.tilesOrder[index]
+        path = os.path.join(self.px.curFolder.path, tile.name)
+        f = open(path, "rb")
+        self.fullImg = Image.open(f)
+        self.fullImg.load()
+        f.close()
+        self.fullImg = pic.fix_image_orientation(self.fullImg)
+        self.zoom = 1.0
+        self.draw_image()
+        self.set_prev_next_stop()
+        self.viewer.set_status(tile.name)
+
+    def draw_image(self):
+        cw = self.canvas.winfo_width()
+        ch = self.canvas.winfo_height()
+        if cw > 1 and ch > 1 and self.fullImg is not None:
+            fw, fh = self.fullImg.size
+            w = cw
+            h = fh * (cw / fw)
+            if h > ch:
+                h = ch
+                w = fw * (ch / fh)
+            zw = w * self.zoom
+            zh = h * self.zoom
+            mx = zw / 2.0
+            my = zh / 2.0
+            mw = cw / 2.0
+            mh = ch / 2.0
+            x0 = min(max(mx - mw + self.panx, 0), max(zw-cw, 0))
+            y0 = min(max(my - mh + self.pany, 0), max(zh-ch, 0))
+            bb = (int(x0), int(y0), int(x0+cw), int(y0+ch))
+            self.cropImg = self.fullImg.resize((int(zw), int(zh))).crop(bb)
+            self.tkPhoto = ImageTk.PhotoImage(self.cropImg)
+            if self.imgItem is not None:
+                self.canvas.delete(self.imgItem)
+            self.imgItem = self.canvas.create_image(0, 0, image=self.tkPhoto, anchor=NW)
+
+    def do_prev(self):
+        """when Prev button clicked"""
+        self.canvas.focus_set()
+        if self.index > 0:
+            for i in range(self.index-1, -1, -1):
+                if isinstance(self.px.tilesOrder[i], PxTilePic):
+                    self.set_picture(i)
+                    self.px.goto_index(i, self.selectColor)
+                    return
+        self.set_prev_next_stop(prev=True)
+
+    def do_next(self):
+        """when Next button clickec"""
+        self.canvas.focus_set()
+        if self.index < len(self.px.tilesOrder) - 1:
+            for i in range(self.index+1, len(self.px.tilesOrder), 1):
+                if isinstance(self.px.tilesOrder[i], PxTilePic):
+                    self.set_picture(i)
+                    self.px.goto_index(i, self.selectColor)
+                    return
+        self.set_prev_next_stop(next=True)
+
+    def set_prev_next_stop(self, prev=False, next=False):
+        self.prevButton.configure(style="Stop.TButton" if prev else "TButton")
+        self.nextButton.configure(style="Stop.TButton" if next else "TButton")
+
 class Viewer(LogHelper, WidgetHelper):
     def __init__(self, px):
         self.env = {}
@@ -25,20 +172,18 @@ class Viewer(LogHelper, WidgetHelper):
 
         # create top level window
         self.top = Toplevel()
+        self.top.geometry("800x800")
         self.myName = "Viewer {:d}".format(self.instNum)
         self.top.title(self.myName)
         self.top.bind('<Destroy>', self.on_destroy)
+        self.top.grid_columnconfigure(0, weight=1)
+        self.top.grid_rowconfigure(2, weight=1)
 
         # create top button bar
         self.topBar = Frame(self.top)
         self.topBar.grid(column=0, row=0, sticky=(N,W,E))
-        self.prevButton = ttk.Button(self.topBar, text="<<", command=self.do_prev)
-        self.prevButton.pack(side=LEFT)
-        self.nextButton = ttk.Button(self.topBar, text=">>", command=self.do_next)
-        self.nextButton.pack(side=LEFT)
         self.splitButton = ttk.Button(self.topBar, text="Split", command=self.do_split)
         self.splitButton.pack(side=RIGHT)
-        self.enable_buttons(True)
 
         # create status bar
         self.statusBar = Frame(self.top)
@@ -55,30 +200,14 @@ class Viewer(LogHelper, WidgetHelper):
         # style for prev/next button hitting limit
         s.configure('Stop.TButton', background='red')
 
-        # create canvas
-        self.canvas = Canvas(self.top, background="black", width=800, height=800)
-        self.canvas.grid(column=0, row=2, sticky=(N,W,E,S))
-        self.top.grid_columnconfigure(0, weight=1)
-        self.top.grid_rowconfigure(2, weight=1)
-
-        # canvas stuff
-        self.canvas.bind('<Configure>', self.on_canvas_resize)
-        self.canvas.bind('<MouseWheel>', self.on_canvas_wheel)
-        self.canvas.bind('<Double-Button-1>', self.on_canvas_doubleclick)
-        self.canvas.bind('<B1-Motion>', self.on_canvas_motion)
-        self.canvas.bind('<ButtonRelease>', self.on_canvas_release)
+        # create left pane
+        self.canvasFrame = Frame(self.top)
+        self.canvasFrame.grid(column=0, row=2, sticky=(N,W,E,S))
+        self.lpane = Pane(self, LEFT)
+        self.rpane = None
+        self.vsplit = False
 
         self.lastError = ""
-        self.index = 0
-        self.fullImg = None
-        self.cropImg = None
-        self.tkPhoto = None
-        self.imgItem = None
-        self.zoom = 1.0
-        self.panx = 0
-        self.pany = 0
-        self.dragging = False
-        self.dragStart = None
         self.set_status_default_or_error()
         instances.append(self)
 
@@ -153,111 +282,21 @@ class Viewer(LogHelper, WidgetHelper):
         """when Log button clicked"""
         self.open_log_window("Log - {}".format(self.myName))
 
-    def enable_buttons(self, enable=True):
-        """enable/disable buttons"""
-        self.enable_widget(self.splitButton, enable)
-        self.enable_widget(self.prevButton, enable)
-        self.enable_widget(self.nextButton, enable)
-
     def do_split(self):
         """when Split button is pressed"""
-        pass
-
-    def on_canvas_resize(self, event):
-        """when user resizes the window"""
-        self.draw_image()
+        if self.rpane is None:
+            self.rpane = Pane(self, RIGHT)
+            self.rpane.set_picture(self.lpane.index)
+        else:
+            self.vsplit = not self.vsplit
+        if self.vsplit:
+            self.lpane.canvas.pack(side=TOP, fill=BOTH, expand=True)
+            self.rpane.canvas.pack(side=TOP, fill=BOTH, expand=True)
+            self.splitButton.configure(text="Horizontal")
+        else:
+            self.lpane.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+            self.rpane.canvas.pack(side=LEFT, fill=BOTH, expand=True)
+            self.splitButton.configure(text="Vertical")
 
     def set_picture(self, index):
-        self.index = index
-        tile = self.px.tilesOrder[index]
-        path = os.path.join(self.px.curFolder.path, tile.name)
-        f = open(path, "rb")
-        self.fullImg = Image.open(f)
-        self.fullImg.load()
-        f.close()
-        self.fullImg = pic.fix_image_orientation(self.fullImg)
-        self.zoom = 1.0
-        self.draw_image()
-        self.set_prev_next_stop()
-        self.set_status(tile.name)
-
-    def draw_image(self):
-        fw, fh = self.fullImg.size
-        cw = self.canvas.winfo_width()
-        ch = self.canvas.winfo_height()
-        if cw > 1 and ch > 1:
-            w = cw
-            h = fh * (cw / fw)
-            if h > ch:
-                h = ch
-                w = fw * (ch / fh)
-            zw = w * self.zoom
-            zh = h * self.zoom
-            mx = zw / 2.0
-            my = zh / 2.0
-            mw = cw / 2.0
-            mh = ch / 2.0
-            x0 = min(max(mx - mw + self.panx, 0), max(zw-cw, 0))
-            y0 = min(max(my - mh + self.pany, 0), max(zh-ch, 0))
-            bb = (int(x0), int(y0), int(x0+cw), int(y0+ch))
-            self.cropImg = self.fullImg.resize((int(zw), int(zh))).crop(bb)
-            self.tkPhoto = ImageTk.PhotoImage(self.cropImg)
-            if self.imgItem is not None:
-                self.canvas.delete(self.imgItem)
-            self.imgItem = self.canvas.create_image(0, 0, image=self.tkPhoto, anchor=NW)
-
-    def on_canvas_wheel(self, event):
-        zbefore = self.zoom
-        self.zoom += event.delta / 2000.0
-        if self.zoom <= 0:
-            self.zoom = zbefore
-        else:
-            self.panx *= self.zoom / zbefore
-            self.pany *= self.zoom / zbefore
-            self.draw_image()
-
-    def on_canvas_doubleclick(self, event):
-        self.zoom = 1.0
-        self.panx = 0
-        self.pany = 0
-        self.draw_image()
-
-    def on_canvas_motion(self, event):
-        """when mouse moved with button down"""
-        if not self.dragging:
-            self.dragging = True
-            self.dragStart = (self.panx + event.x, self.pany + event.y)
-            self.canvas.configure(cursor="fleur")
-        else:
-            self.panx = self.dragStart[0] - event.x
-            self.pany = self.dragStart[1] - event.y
-            self.draw_image()
-
-    def on_canvas_release(self, event):
-        """when mouse button released"""
-        self.dragging = False
-        self.canvas.configure(cursor="")
-
-    def do_prev(self):
-        """when Prev button clicked"""
-        if self.index > 0:
-            for i in range(self.index-1, -1, -1):
-                if isinstance(self.px.tilesOrder[i], PxTilePic):
-                    self.set_picture(i)
-                    self.px.goto_index(i, len(selectColors))
-                    return
-        self.set_prev_next_stop(prev=True)
-
-    def do_next(self):
-        """when Next button clickec"""
-        if self.index < len(self.px.tilesOrder) - 1:
-            for i in range(self.index+1, len(self.px.tilesOrder), 1):
-                if isinstance(self.px.tilesOrder[i], PxTilePic):
-                    self.set_picture(i)
-                    self.px.goto_index(i, len(selectColors))
-                    return
-        self.set_prev_next_stop(next=True)
-
-    def set_prev_next_stop(self, prev=False, next=False):
-        self.prevButton.configure(style="Stop.TButton" if prev else "TButton")
-        self.nextButton.configure(style="Stop.TButton" if next else "TButton")
+        self.lpane.set_picture(index)
