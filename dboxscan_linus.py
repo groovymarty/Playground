2f2b56edcc6bb6bdc993390c808baa9d36d668c5
Marty Sauser <msauser2@gmail.com>
2021-01-24 14:56:11 -0500

    Add dbox backup script for linus

---
+++ dboxscan_linus.py
@@ -0,0 +1,31 @@
+import dboxscan, os
+
+basedir = os.path.join("D:\\Users","Msaus")
+dboxdir = os.path.join(basedir,"DropBox")
+
+def scanone(srctail, targtail):
+    dboxscan.scandir(os.path.join(basedir, srctail), os.path.join(dboxdir, targtail))
+
+scanone("Documents\\Business", "Marty\\Business")
+scanone("Documents\\Church", "Marty\\Church")
+scanone("Documents\\Downloads", "Marty\\Downloads")
+scanone("Documents\\Family Archives", "Family Archives")
+scanone("Documents\\Family History", "Family History")
+scanone("Documents\\Grace", "Grace")
+scanone("Documents\\Heidi", "Heidi")
+scanone("Documents\\House", "House")
+scanone("Documents\\Jeff", "Jeff")
+scanone("Documents\\Kent Singers", "Kent Singers")
+scanone("Documents\\Kermit", "Kermit")
+scanone("Documents\\Lawsuit", "Lawsuit")
+scanone("Documents\\Letters", "Marty\\Letters")
+scanone("Documents\\Martin", "Martin")
+scanone("Documents\\Organ", "Organ")
+scanone("Documents\\Past Projects", "Marty\\Past Projects")
+scanone("Documents\\SlideShows", "SlideShows")
+scanone("Documents\\Various", "Marty\\Various")
+scanone("Pictures", "Pictures")
+scanone("Documents\\St Lukes Archives 2014", "St Lukes Archives 2014")
+scanone("Music\\iTunes", "iTunes")
+
+dboxscan.finish()
