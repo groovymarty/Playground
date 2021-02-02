12f92c7a23f2618640071c106b6adb0fb80b34bf
Marty Sauser <msauser2@gmail.com>
2021-02-01 19:16:29 -0500

    Fix Desktop path

--- dboxscan_lucy.py
+++ dboxscan_lucy.py
@@ -7,9 +7,10 @@ def scanone(srctail, targtail, exclude=[]):
     dboxscan.scandir(os.path.join(basedir, srctail), os.path.join(dboxdir, targtail), exclude)
 
 scanone("Documents", os.path.join("Jill", "Documents"), ["GitHub", "Life", "Teaching"])
-scanone("Desktop", os.path.join("Jill", "Desktop_lucy"))
 basedir = os.path.join(basedir, "Documents")
 scanone("Life", "Life")
 scanone("Teaching", "Teaching")
+basedir = os.path.join("C:\\Users","jills")
+scanone("Desktop", os.path.join("Jill", "Desktop_lucy"))
 
 dboxscan.finish()
