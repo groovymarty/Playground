ac877d7c62c2bb26e7250242e6064b0c7867f05b
Marty Sauser <msauser2@gmail.com>
2021-02-18 16:48:07 -0500

    Add Archives folder

--- dboxscan_lucy.py
+++ dboxscan_lucy.py
@@ -6,8 +6,9 @@ dboxdir = os.path.join(basedir,"DropBox")
 def scanone(srctail, targtail, exclude=[]):
     dboxscan.scandir(os.path.join(basedir, srctail), os.path.join(dboxdir, targtail), exclude)
 
-scanone("Documents", os.path.join("Jill", "Documents"), ["GitHub", "Life", "Teaching"])
+scanone("Documents", os.path.join("Jill", "Documents"), ["Archives", "GitHub", "Life", "Teaching"])
 basedir = os.path.join(basedir, "Documents")
+scanone("Archives", "Archives")
 scanone("Life", "Life")
 scanone("Teaching", "Teaching")
 basedir = os.path.join("C:\\Users","jills")
